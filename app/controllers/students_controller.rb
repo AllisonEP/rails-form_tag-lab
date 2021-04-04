51bf5b8105baeffe3166909cde8507046e436b5c
Allison Perry <allisonpshortnorth@gmail.com>
2021-04-04 17:24:59 -0400

    commit tree 813d35a0118bead23272e5dec238a15c8cd91419

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,17 @@
 class StudentsController < ApplicationController
+
+  def create
+    @student= Student.create(first_name: params[:student][:first_name],last_name: params[:student][:last_name])
+    redirect_to students_path
+  end 
   
+  def new 
+    @student = Student.new
+  end 
+
   def index
     @students = Student.all
+    
   end
 
   def show
